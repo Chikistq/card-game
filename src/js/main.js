bc4433132fafb9f86ca87a493bdbd7ff5334061f
Chikistq <Evesecurity1>
2022-09-22 20:16:05 +0300

    доработка генерации массива

--- src/js/main.js
+++ src/js/main.js
@@ -3,17 +3,15 @@ import {app} from './_Card.js'
 
 document.addEventListener('DOMContentLoaded', () => {
 
-  const arr = [1, 1, 2, 2, 3, 3, 4, 4, 5, 5, 6, 6, 7, 7, 8, 8]
+
+  function getArray(length) {
+    return [...Array.from(Array(length+1).keys()).slice(1), ...Array.from(Array(length+1).keys()).slice(1)]
+  }
+
+  const arr = getArray(8)
+
 
   const cards = app('#cards-app', arr)
   cards.shuffle()
   cards.create()
-
-
-
-
-
-
-
-
 })
